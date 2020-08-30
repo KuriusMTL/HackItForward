2fec25a3773db2c2dc32039065f73c2d4cb1088b
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-08-30 15:47:27 -0400

    Blackify mistune

--- app/templatetags/mistune.py
+++ app/templatetags/mistune.py
@@ -4,6 +4,7 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.filter(name="mistune", is_safe=True)
 def mistune_html_no_highlight(text):
     markdown = mistune.create_markdown(
