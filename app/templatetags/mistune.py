be7cb5c42b99d3b9c39df7c44843e96dd882a43a
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-08-30 15:45:14 -0400

    Add markdown rendering for challenge/project descriptions

---
+++ app/templatetags/mistune.py
@@ -0,0 +1,12 @@
+import mistune
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(name="mistune", is_safe=True)
+def mistune_html_no_highlight(text):
+    markdown = mistune.create_markdown(
+        plugins=["strikethrough", "footnotes", "table"],
+    )
+    return mark_safe(markdown(text))
--- stratus/settings.py
+++ stratus/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'app',
 
     'colorfield',
+
+    'mistune',
 ]
 
 MIDDLEWARE = [
