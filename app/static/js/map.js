04f50834d63234249cd0015e72af89e6816ca813
aava <aavachhori@gmail.com>
2020-08-30 03:04:48 -0400

    maps

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,21 +1,23 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibmlraXN1IiwiYSI6ImNrZWdla2NvOTB1MWIyeHVxN3lrZmExb3EifQ._NPc57J623HhSRubXYPcIg';
-
-var coordinates = document.getElementById('coordinates');
-var lngLat; 
-
+var coordinates = document.getElementById('map');
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
-    center: [0, 0],
-    zoom: 2
+container: 'map',
+style: 'mapbox://styles/mapbox/dark-v10',
+center: [0, 0],
+zoom: 2
 });
  
 var marker = new mapboxgl.Marker({
-    draggable: true
-}).setLngLat([0, 0]).addTo(map);
+draggable: true
+})
+.setLngLat([0, 0])
+.addTo(map);
  
 function onDragEnd() {
-    lngLat = marker.getLngLat();
+var lngLat = marker.getLngLat();
+// coordinates.style.display = 'block';
+// coordinates.innerHTML =
+// 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
 }
  
 marker.on('dragend', onDragEnd);
