243bcd62f47e3dae36198fef7d51a43849b9fb3f
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-09-14 14:31:32 -0400

    Alphabetize new imports

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class SocialLink(models.Model):
