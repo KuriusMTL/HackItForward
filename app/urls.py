7bde26a6d83eddbd9fab7e3fc5b5e5d8b8938b54
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-09-19 18:10:33 -0400

    Redirect user on `/login/` if already authenticated

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("register/", views.RegisterView.as_view(), name="register"),
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html", redirect_authenticated_user=True), name="login"),
     path("login/social/", include("social_django.urls", namespace="social")),
     path("logout/", auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     # TODO: Rename this to fit in line with project and challenge style.
