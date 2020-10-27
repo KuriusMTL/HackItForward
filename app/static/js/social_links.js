efafb27135cf81dc527a7093cc22fb4ef3b7bc92
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-10-26 21:33:25 -0400

    Update social_links.js

--- app/static/js/social_links.js
+++ app/static/js/social_links.js
@@ -1,37 +1,48 @@
+// Changes the value of `id_${FORM_PREFIX}-TOTAL_FORMS`, if a valid argument d (delta) is given,
+// and returns said value
+const total_forms_val = d => {
+	if (d === undefined || isNaN(parseInt(d))) d = 0;
+	const elm = $(`#id_${FORM_PREFIX}-TOTAL_FORMS`);
+	return elm.val(parseInt(elm.val())+d).val();
+} 
+
 $(window).on("load", () => {
 	$(".social-del").click(toggleSocialInputDelete);
 	$(".social-add").click(e => {
 		e.preventDefault();
 		
-		const form_index = $(`#id_${FORM_PREFIX}-TOTAL_FORMS`).val();
-		const new_form = $($("#social_form_empty").html().replace(/__prefix__/g, form_index));
+		const new_form = $($("#social_form_empty").html().replace(/__prefix__/g, total_forms_val()));
 		$(".social-inputs").append(new_form);
-		$(`#id_${FORM_PREFIX}-TOTAL_FORMS`).val((form_index/1)+1);
+		total_forms_val(1);
 		new_form.children(".social-del").click(toggleSocialInputDelete);
 	});
 
-
-	// enable after the click handler has been added
+	// A protection against those with especially fast fingers that can press buttons before the script loads
 	$(".social-add, .social-del").prop("disabled", false);
 });
-
-const CLOSE = "delete";
-const OPEN = "time-restore-setting";
 
 function toggleSocialInputDelete(e) {
 	e.preventDefault();
 
 	if (parseInt($(this).attr("data-id").split("-")[1]) >= parseInt($(`#id_${FORM_PREFIX}-INITIAL_FORMS`).val())) {
+		total_forms_val(-1);
+
+		// Updates the next input elements' numeric indices to account for the removal
+		$(this).parent().nextAll().children().each(function() {
+			const prefix = $(this).attr("id") === undefined ? "data-" : "";
+			const split_id = $(this).attr(prefix+"id").split("-");
+			const split_name = $(this).prop("name").split("-");
+			split_id[1] = split_name[1] = parseInt(split_id[1])-1;
+			$(this).attr(prefix+"id",split_id.join("-"));
+			$(this).prop("name",split_name.join("-"));
+		});
 		$(this).parent().remove();
-		$(`#id_${FORM_PREFIX}-TOTAL_FORMS`).val(parseInt($(`#id_${FORM_PREFIX}-TOTAL_FORMS`).val())-1);
 		return;
 	}
 
-	$(this).parent().children().filter(function() {
-		return this.classList.length === 0 || !this.classList.contains("social-del")
-	}).each((ind,elm) => $(elm).toggleClass("disabled"));
+	$(this).siblings().each((ind,elm) => $(elm).toggleClass("disabled"));
 
 	const checkbox = $('#'+$(this).attr("data-id"))[0];
 	checkbox.checked = !checkbox.checked;
-	$(this).html(`<i class="zmdi zmdi-${$(this).html().includes(CLOSE) ? OPEN : CLOSE}" aria-hidden="true"></i>`);
+	$(this).children("i").toggleClass("zmdi-delete").toggleClass("zmdi-time-restore-setting")
 }
