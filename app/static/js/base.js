8e45b0720ad91fe6d0cecf944a71e6c88516aa90
Miniapple8888 <miniapple8888@gmail.com>
2021-03-26 22:39:06 -0400

    Scrolling social-float improved + Responsive

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -34,8 +34,15 @@ function checkOffset() {
   if($('#social-float').offset().top + $('#social-float').height()
                                          >= $('#footer').offset().top - 10)
       $('#social-float').css('position', 'absolute');
-  if($(document).scrollTop() + window.innerHeight < $('#footer').offset().top)
+  if($(document).scrollTop() + window.innerHeight < $('#footer').offset().top && $(document).scrollTop() >= 84) {
       $('#social-float').css('position', 'fixed'); // restore when you scroll up
+      $('#social-float').css('top', '25px');
+  }
+  if($(document).scrollTop() < 84) {
+    console.log("printed");
+    $('#social-float').css('position', 'absolute');
+    $('#social-float').css('top', '145px');
+  }
 }
 
 function openTab(evt, level) {
