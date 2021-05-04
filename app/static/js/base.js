33e6c0e524ed929310355f3d8e4352d71239b478
Steven Gong <gong.steven@hotmail.com>
2021-05-03 21:40:58 -0400

    update dropdown options

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -239,8 +239,8 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active-dot", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " active-dot";
 }
