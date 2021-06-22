6ce690343a37e1693028057cbf1fd08b9372b007
Miniapple8888 <miniapple8888@gmail.com>
2021-06-22 00:34:16 -0400

    fix mistune

--- app/templatetags/mistune.py
+++ app/templatetags/mistune.py
@@ -5,7 +5,4 @@ from django.utils.safestring import mark_safe
 
 @library.filter
 def mistune_html_no_highlight(text):
-    markdown = mistune.Markdown(
-        plugins=["strikethrough", "footnotes", "table"],
-    )
-    return mark_safe(markdown(text))
+    return mark_safe(mistune.markdown(text))
